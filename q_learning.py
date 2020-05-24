1c4b38dce32d64189a9c089c50d22fcdfac54f80
Evyatar Segal <>
2020-05-23 23:07:41 -0400

    Fixes to learning parameters - performance is much better now

--- q_learning.py
+++ q_learning.py
@@ -2,24 +2,27 @@ import gym
 from gym.envs.toy_text.frozen_lake import generate_random_map
 import numpy as np
 
-LEARNING_RATE = 0.1
-
-DISCOUNT = 0.95
-EPISODES = 50000
+DISCOUNT = 0.99
+EPISODES = 100000
 SHOW_EVERY = 5000
+RESTART_DECAY_EVERY = 10000
 
 # Exploration settings
 INITIAL_EPSILON = 1
 INITIAL_LEARNING_RATE = 0.8
-MIN_EPSILON = 0
+MIN_EPSILON = 0.05
 MIN_LEARNING_RATE = 0.1
-DECAY_RATE = 0.99
+
+# Exploration decay
+EPISODE_TO_END_DECAY = (EPISODES * 3) // 4
+EPSILON_DECAY_FACTOR = (MIN_EPSILON / INITIAL_EPSILON) ** (1 / EPISODE_TO_END_DECAY)
+LEARNING_DECAY_FACTOR = (MIN_LEARNING_RATE / INITIAL_LEARNING_RATE) ** (1 / EPISODE_TO_END_DECAY)
 
 
 class FrozenLakeAgent(object):
     def __init__(self):
-        random_map = generate_random_map(size=5, p=0.8)
-        self.env = gym.make("FrozenLake-v0", is_slippery=False, desc=random_map)
+        random_map = generate_random_map(size=8, p=0.8)
+        self.env = gym.make("FrozenLake-v0", is_slippery=True, desc=random_map)
         self.env.reset()
 
         self.epsilon = INITIAL_EPSILON
@@ -29,42 +32,54 @@ class FrozenLakeAgent(object):
         self.state_space = [self.env.observation_space.n]
         self.q_table = np.random.uniform(low=-2, high=0, size=(self.state_space + [self.action_space]))
 
-    def _run_single_episode(self, episode):
+    def _run_single_episode(self, episode, only_follow_best_route=False):
         state = self.env.reset()
 
         done = False
         while not done:
-            if np.random.random() > self.epsilon:
+            if only_follow_best_route:
                 action = np.argmax(self.q_table[state])
+                self.env.render()
             else:
-                action = self.env.action_space.sample()
+                if np.random.random() <= self.epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    action = np.argmax(self.q_table[state])
 
             new_state, reward, done, _ = self.env.step(action)
 
             # Make the Q-learning computations
             max_future_q = np.max(self.q_table[new_state])
             current_q = self.q_table[state, action]
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            new_q = (1 - self.learning_rate) * current_q + self.learning_rate * (reward + DISCOUNT * max_future_q)
             self.q_table[state, action] = new_q
 
             state = new_state
 
         if reward > 0:
             print(f"{episode} - Won!")
-        elif episode % SHOW_EVERY == 0:
-            print(episode)
+
+        if only_follow_best_route:
             self.env.render()
-            print()
-            self.epsilon = INITIAL_EPSILON
-            self.learning_rate = INITIAL_LEARNING_RATE
+        else:
+            if episode % SHOW_EVERY == 0:
+                print(episode)
+                self.env.render()
+                print()
+
+            if episode % RESTART_DECAY_EVERY == 0:
+                self.epsilon = INITIAL_EPSILON
+                self.learning_rate = INITIAL_LEARNING_RATE
 
     def run_episodes(self, num_episodes):
         for episode in range(num_episodes):
             self._run_single_episode(episode)
 
             # Decay epsilon and learning rate
-            self.epsilon = max(MIN_EPSILON, self.epsilon * DECAY_RATE)
-            self.learning_rate = max(MIN_LEARNING_RATE, self.learning_rate * DECAY_RATE)
+            self.epsilon = max(MIN_EPSILON, self.epsilon * EPSILON_DECAY_FACTOR)
+            self.learning_rate = max(MIN_LEARNING_RATE, self.learning_rate * LEARNING_DECAY_FACTOR)
+
+        self._run_single_episode(0, only_follow_best_route=True)
 
 
 if __name__ == "__main__":
